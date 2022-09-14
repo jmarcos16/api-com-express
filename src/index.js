adfce7f687b0d66f7756a8deb5352bda3839c12e
jmarcos16 <contato.marcosrf@gmail.com>
2022-09-14 11:08:12 -0300

    Implementação: Liberação de cors, para realizar requisições de outro dominio

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const routerApp = require("./routes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.APP_PORT || 9001;
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routerApp);
 app.listen(PORT, () =>
